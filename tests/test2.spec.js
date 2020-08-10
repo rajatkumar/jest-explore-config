af747ee92addc25e1ad7c867c6200656868f7131
Rajat Kumar <rajatk@netflix.com>
2020-08-10 14:00:06 -0700

    chore: update code

--- tests/test2.spec.js
+++ tests/test2.spec.js
@@ -2,14 +2,22 @@ const { getTestNameFromFilepath, log } = require('../helpers');
 const NAME = getTestNameFromFilepath(__filename);
 describe(`${NAME}`, function() {
     beforeEach(function() {
-        log(`${NAME} beforeEach`);
+        log(
+            `${NAME} beforeEach - value of NODE_DEBUG: ${global.__NODE_DEBUG__}`
+        );
     });
 
     afterEach(function() {
-        log(`${NAME} afterEach`);
+        // this will show the modified value
+        log(
+            `${NAME} afterEach - value of NODE_DEBUG: ${global.__NODE_DEBUG__}`
+        );
     });
 
     it('tests 1', function() {
         log(`${NAME} it`);
+        log(`${NAME} Current value of NODE_DEBUG: ${global.__NODE_DEBUG__}`);
+        global.__NODE_DEBUG__ = 'WHAT_THE_JEST-TEST2';
+        log(`${NAME} New value of NODE_DEBUG: ${global.__NODE_DEBUG__}`);
     });
 });
